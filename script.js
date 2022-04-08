9d9b46f68bb99785ae8b6d676c365b16ae579688
JoelNymark <89513327+JoelNymark@users.noreply.github.com>
2022-04-08 16:05:27 +0200

    test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibXl0aGljYWx3aW50ZXIiLCJhIjoiY2wxcTlpYjJtMWl6ZTNkbzJhb2h0b2MyNCJ9.X-E8P4L-bWnDYn1lcpDCggeyJ1IjoibXl0aGljYWx3aW50ZXIiLCJhIjoiY2t6dmQ1eXd2MDF2YjJubnU0dndreXlncSJ9.0MrTHv0a-A9pk.eyJ1IjoibXl0aGljYWx3aW50ZXIiLCJhIjoiY2wxcTlpYjJtMWl6ZTNkbzJhb2h0b2MyNCJ9.X-E8P4L-bWnDYn1lcpDCggJSoYgceHt6w';
+mapboxgl.accessToken = 'pk.eyJ1IjoibXl0aGljYWx3aW50ZXIiLCJhIjoiY2t6dmQ1eXd2MDF2YjJubnU0dndreXlncSJ9.0MrTHv0a-A9JSoYgceHt6w';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mythicalwinter/ckzvd81r6000f15mf7tzs88g3', // style URL  mapbox://styles/mythicalwinter/ckzvd81r6000f15mf7tzs88g3  // <---s\ dark vertion of the map
