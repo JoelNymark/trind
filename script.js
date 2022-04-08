85d58f961c183d384ea61fddb181a0cd3de0b45b
JoelNymark <89513327+JoelNymark@users.noreply.github.com>
2022-04-08 13:11:20 +0200

    test av mapbox map apikey

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibXl0aGljYWx3aW50ZXIiLCJhIjoiY2t6dmQ1eXd2MDF2YjJubnU0dndreXlncSJ9.0MrTHv0a-A9JSoYgceHt6w';
+mapboxgl.accessToken = 'pk.eyJ1IjoibXl0aGljYWx3aW50ZXIiLCJhIjoiY2wxcTlpYjJtMWl6ZTNkbzJhb2h0b2MyNCJ9.X-E8P4L-bWnDYn1lcpDCggeyJ1IjoibXl0aGljYWx3aW50ZXIiLCJhIjoiY2t6dmQ1eXd2MDF2YjJubnU0dndreXlncSJ9.0MrTHv0a-A9pk.eyJ1IjoibXl0aGljYWx3aW50ZXIiLCJhIjoiY2wxcTlpYjJtMWl6ZTNkbzJhb2h0b2MyNCJ9.X-E8P4L-bWnDYn1lcpDCggJSoYgceHt6w';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL  mapbox://styles/mythicalwinter/ckzvd81r6000f15mf7tzs88g3  // <---s\ dark vertion of the map
+    style: 'mapbox://styles/mythicalwinter/ckzvd81r6000f15mf7tzs88g3', // style URL  mapbox://styles/mythicalwinter/ckzvd81r6000f15mf7tzs88g3  // <---s\ dark vertion of the map
     center: [17.875592712053653, 59.623533595854944], // starting position [lng, lat]
     //add marker to cords above.
     zoom: 15 // starting zoom
